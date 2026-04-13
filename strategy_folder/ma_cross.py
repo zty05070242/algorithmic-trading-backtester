539781edb5d5f7cc38b7ec09067f1fbe75dde568
zty05070242 <zty05070242@gmail.com>
2026-04-13 14:58:37 +0000

    260413_update_sys_fix

--- strategy_folder/ma_cross.py
+++ strategy_folder/ma_cross.py
@@ -1,4 +1,8 @@
-from strategy_folder._strategy_bass_class import Strategy
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from _strategy_bass_class import Strategy
 from data_loader import load_historical_data
 import pandas as pd
 
